2fe0f8e11657256be4cc87c6393bddb11d1e5fa3
Nick Plucker <pluck011@morris.umn.edu>
2018-11-08 10:47:16 -0600

    Implement four nested classes to calculate each part of the MPD triangle
    Inside ThreadedMinimumPairwiseDistance, there are four nest classes: LowerLeftMPD, BottomRightMPD, TopRightMPD and CenterMPD. Each nested class calculates each part of the MPD triangle (as described in the README) and compares the local result of each class to the global result. If the local result is smaller than the global result, the global result is then the local result (aka the minimum pairwise distance). All tests pass.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,125 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+    public long globalResult = Integer.MAX_VALUE;
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+        Thread lowerLeftMPD = new Thread(new LowerLeftMPD(values));
+        Thread bottomRightMPD = new Thread(new BottomRightMPD(values));
+        Thread topRightMPD = new Thread(new TopRightMPD(values));
+        Thread centerMPD = new Thread(new CenterMPD(values));
+
+        lowerLeftMPD.start();
+        bottomRightMPD.start();
+        topRightMPD.start();
+        centerMPD.start();
+
+        try {
+            lowerLeftMPD.join();
+            bottomRightMPD.join();
+            topRightMPD.join();
+            centerMPD.join();
+        } catch (InterruptedException e) {
+            System.exit(1);
+        }
+
+        return globalResult;
+    }
+
+    public void updateGlobalResult(long localResult) {
+        if(localResult < globalResult) {
+            globalResult = localResult;
+        }
+    }
+
+
+    public class LowerLeftMPD implements Runnable {
+        public int[] values;
+
+        public LowerLeftMPD (int[] values) {
+            this.values = values;
+        }
+
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = 0; i < values.length/2; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+
+            updateGlobalResult(localResult);
+        }
+    }
+
+    public class BottomRightMPD implements Runnable {
+        public int[] values;
+
+        public BottomRightMPD (int[] values) {
+            this.values = values;
+        }
+
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = 0; j < i - (values.length/2); ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+
+            updateGlobalResult(localResult);
+        }
+    }
+
+    public class TopRightMPD implements Runnable {
+        public int[] values;
+
+        public TopRightMPD (int[] values) {
+            this.values = values;
+        }
+
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = values.length/2; j < i; ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+
+            updateGlobalResult(localResult);
+        }
+    }
+
+    public class CenterMPD implements Runnable {
+        public int[] values;
+
+        public CenterMPD (int[] values) {
+            this.values = values;
+        }
+
+        public void run() {
+            long localResult = Integer.MAX_VALUE;
+            for (int i = values.length/2; i < values.length; ++i) {
+                for (int j = i - (values.length/2); j < i; ++j) {
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < localResult) {
+                        localResult = diff;
+                    }
+                }
+            }
+
+            updateGlobalResult(localResult);
+        }
     }
 
 }
